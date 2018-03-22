37cb2941891992c744d58042a335b3c8f82806a1
Jarideno <jdenottelander@gmail.com>
2018-03-22 12:19:45 +0100

    Revert "Log regel toegevoegd"
    This reverts commit 0f76c08dfc4210c6ef9bf3bad145eda29fc22105.

--- app/src/main/java/nl/avans/informatica/bioscoopapp/MainActivity.java
+++ app/src/main/java/nl/avans/informatica/bioscoopapp/MainActivity.java
@@ -2,7 +2,6 @@ package nl.avans.informatica.bioscoopapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d("Main", "Test");
     }
 }
