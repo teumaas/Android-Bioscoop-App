b69bb6ac85519dadf9eab21a1c0180e170c93372
Tom Smits <tmsmits@gmail.com>
2018-03-26 13:42:10 +0200

    Testing qr

--- app/src/main/java/nl/avans/informatica/bioscoopapp/MainActivity.java
+++ app/src/main/java/nl/avans/informatica/bioscoopapp/MainActivity.java
@@ -1,13 +1,32 @@
 package nl.avans.informatica.bioscoopapp;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView qrImage;
+    private File qrGenerate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qrGenerate = (File) QRCode.from("Hello World").file();
+        qrImage = (ImageView) findViewById(R.id.qrCode);
+
+        Picasso.with(this)
+                .load(qrGenerate.toString())
+                .into(qrImage);
     }
 }
